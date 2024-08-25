52dc83187c3629d8526815014f5f1bba5b968616
Alexander Moreno <am@alexandermoreno.com>
2024-08-25 01:15:07 -0500

    Update Configuration.cs

--- ToolboxApp.Features/Configuration.cs
+++ ToolboxApp.Features/Configuration.cs
@@ -7,6 +7,7 @@ public static class FeatureConfiguration
     
     public static IServiceCollection AddFeatures(this IServiceCollection services)
     {
+        // Add MediatR
         services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(typeof(FeatureConfiguration).Assembly));
         return services;
     }
